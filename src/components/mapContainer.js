78bc33e7113c9898a4200248989a6ed1c9db6433
Ninfa Niño <ninfa_sys@hotmail.com>
2019-08-08 13:51:51 -0500

    first commit

--- src/components/mapContainer.js
+++ src/components/mapContainer.js
@@ -7,7 +7,14 @@ class MapContainer extends Component {
     render() {
         
         return (
-          <Map google={this.props.google} zoom={14}>
+          <Map 
+            google={this.props.google} 
+            zoom={14}
+            initialCenter={{
+              lat: 20.656462, 
+              lng: -103.400406
+            }}
+          >
             <Marker name={'Manofacturing'} />
           </Map>
         );
